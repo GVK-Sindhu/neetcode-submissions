96ffdb48a03bb466c5cfbf629708392688733f15
GANTADA VENKATA KAVYA SINDHU <23A91A0595@aec.edu.in>
2026-07-15 12:51:52 +0530

    Add: binary-search - submission-0

---
+++ Data Structures & Algorithms/binary-search/submission-0.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int tar) {
+        int l=0,h=nums.size()-1;
+        while(l<=h){
+            int mid=l+(h-l)/2;
+            if(nums[mid]==tar){
+                return mid;
+            }
+            else if(nums[mid]>tar){
+                h=mid-1;
+            }
+            else{
+                l=mid+1;
+            }
+        }
+        return -1;
+    }
+};
